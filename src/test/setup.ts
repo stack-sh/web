d2c3568dc4e3d42e59cc182a0ee24beee4452760
JJ <junya03dance@gmail.com>
2026-09-04 01:36:55 +0900

    Stabilize ResizeObserver-dependent tests (#8)

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -4,6 +4,14 @@ import { afterEach, vi } from "vitest"
 
 afterEach(cleanup)
 
+class ResizeObserverMock {
+  observe = vi.fn()
+  unobserve = vi.fn()
+  disconnect = vi.fn()
+}
+
+vi.stubGlobal("ResizeObserver", ResizeObserverMock)
+
 Object.defineProperty(URL, "createObjectURL", {
   configurable: true,
   value: vi.fn(() => "blob:stack-preview"),
